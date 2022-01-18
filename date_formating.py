a6181dd0b2177184f2aeae1d44528fca85bf3264
abdullah_awad <abdullah.alashafi@gmail.com>
2022-01-18 23:23:28 +0300

    solve the date formating with 3 ways

--- date_formating.py
+++ date_formating.py
@@ -1,15 +1,26 @@
 # TODO convert date format yyyy-mm-dd to dd-mm-yyyy
 # example 2020-02-10 to 10-02-2020
 
-
+# 1
 def date_formating(date):
-	import datetime
-	# year = date[:4]
-	# month = date[5:7]
-	# day = date[8:]
-	# return day + "-" + month + "-" + year 
-	date_obj = datetime.datetime.strptime(date, "%Y-%m-%d")
-	out = date_obj.strftime("%d-%m-%Y")
-	return out
+	date = date.strip()
+	year = date[:4]
+	month = date[5:7]
+	day = date[8:]
+	return f"{day}-{month}-{year}"
 
-print(date_formating("1932-09-01"))
+# 2
+from datetime import datetime
+def date_formating(date):
+  date = date.strip()
+  return datetime.strptime(date, "%Y-%m-%d").strftime("%d-%m-%Y")
+
+# 3
+def date_formating(date):
+	date = date.strip()
+	year, month, day = date.split('-')
+	return '-'.join((year, month, day))
+
+
+print(date_formating("1932-09-23"))
+
