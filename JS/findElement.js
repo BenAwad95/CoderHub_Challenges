8c8e4573d4a3cb471589cb99a56a147ac9e97979
Abdullah Awad <abdullah.alashafi@gmail.com>
2022-04-01 11:03:36 +0300

    findElement done

--- JS/findElement.js
+++ JS/findElement.js
@@ -1,1 +1,12 @@
-console.log('Hi')
+function findElement(array, element) {
+    let output = false;
+    for (let index = 0; index < array.length; index++) {
+        if (element == array[index]) {
+            output = true;
+            break;
+        }
+    }
+    return output
+}
+
+console.log(findElement([1, 2, 3], 1))
